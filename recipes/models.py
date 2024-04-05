9110b9ae936f5ec3643ad04b946a9dc23eb48fcf
kateuj <kate.ulloajames@gmail.com>
2024-04-05 21:18:29 +0000

    user model added

--- recipes/models.py
+++ recipes/models.py
@@ -1,2 +1,11 @@
 from recipes import db
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    cookbook = db.relationship('#', backref='user', cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return self.username
