1df0a09db885d7ecf730a5e0741bcc690f9a73d4
kateuj <kate.ulloajames@gmail.com>
2024-04-10 13:30:26 +0000

    js for range picker added

--- recipes/static/js/script.js
+++ recipes/static/js/script.js
@@ -1,3 +1,20 @@
+// Star rating picker
+var slider = document.getElementById('star-rating');
+noUiSlider.create(slider, {
+ start: [0, 0],
+ connect: true,
+ step: 1,
+ orientation: 'horizontal', // 'horizontal' or 'vertical'
+ range: {
+   'min': 0,
+   'max': 5
+ },
+ format: wNumb({
+   decimals: 0
+ })
+});
+
+// Side nav bar on mobile screens
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
