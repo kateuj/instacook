f417c6c346a7e0c0298c54d74beef744359a095d
kateuj <kate.ulloajames@gmail.com>
2024-04-16 19:09:20 +0000

    delete_recipe route error changed - still displayed as delete_cookbook

--- recipes/routes.py
+++ recipes/routes.py
@@ -186,7 +186,7 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe, cookbook=cookbook)
 
 
-@app.route("/delete_cookbook/<int:recipe_id>")
+@app.route("/delete_recipe/<int:recipe_id>")
 def delete_recipe(recipe_id):
     recipe = Recipe.query.get_or_404(recipe_id)
     db.session.delete(recipe)
