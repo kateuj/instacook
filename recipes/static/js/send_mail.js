f95065e936f9db70e410c53ba2908c81e7242789
kateuj <kate.ulloajames@gmail.com>
2024-04-17 21:52:47 +0000

    send mail js logic for contact form

---
+++ recipes/static/js/send_mail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("service_ld8bf5j", "instacook_template", {
+        "user_name": contactForm.user_name.value,
+        "user_email": contactForm.user_email.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            window.open("thank-you.html");
+        },
+        function(error) {
+            console.log("FAILED", error);
+            window.open("404.html");
+        }
+    );
+    return false;  // To block from loading a new page
+}
