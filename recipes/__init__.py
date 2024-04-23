3e9a679b8ecbaad44b17e0740e749679aebd8253
kateuj <kate.ulloajames@gmail.com>
2024-04-23 11:32:42 +0000

    remove comments

--- recipes/__init__.py
+++ recipes/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env  # noqa
+    import env
 
 
 app = Flask(__name__)
@@ -18,5 +18,5 @@ else:
 
 db = SQLAlchemy(app)
 
-from recipes import routes  # noqa
+from recipes import routes
 
